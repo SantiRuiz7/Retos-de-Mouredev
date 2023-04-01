c366cda7fa17e66591902747647f4439dea0f123
Santiago Ruiz <ruizsantiagodavid@gmail.com>
2023-04-01 11:17:15 -0300

    Se comienza retos para practicar Python

---
+++ Retos/0_reto.py
@@ -0,0 +1,28 @@
+### El famoso fizz buzz
+
+"""
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+ """
+
+string1 = "fizz"
+
+string2 = "buzz"
+
+string3 = "fizzbuzz"
+
+for i in range(1, 101):
+    
+    if i % 3 == 0 and i % 5 == 0:
+       print(string3)
+    elif i % 3 == 0 :
+        print(string1)
+    elif i % 5 == 0:
+       print(string2)
+    else: 
+      print(i)
